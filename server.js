8106d47edcccb2386b153a09ee1fa093504e5df1
adamwdennis <adam.dennis@gmail.com>
2012-10-23 20:47:06 -0300

    trello ... not working

--- server.js
+++ server.js
@@ -13,9 +13,31 @@ github.authenticate({
 github.issues.getAllMilestones({
   user: config.user,
   repo: config.repo,
-  state: "open",
-  sort: "completeness"
+  state: "open"
 }, function(err, res) {
   if(err) throw err;
+  res.sort(function(a,b) {
+    return b.number - a.number; 
+  });
+  /*
+  var milestones = res.map(function(el) {
+    return {
+      number: el.number,
+      title: el.title,
+      created: el.created_at
+    };
+  });
+  console.log(milestones);
+  */
+  var latestMilestone = res[0];
+
+});
+
+/*
+github.events.getFromRepoIssues({
+  user: config.user,
+  repo: config.repo
+}, function(err, res) {
   console.log(res);
 });
+*/
