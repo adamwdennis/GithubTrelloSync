f329eaa1932d8714c78fb281a57a6f278dd35d4b
Jeremy Wright <jeremywright@goinstant.com>
2012-11-06 18:11:56 -0400

    Cleanup

--- server.js
+++ server.js
@@ -20,58 +20,10 @@ var dailyBoardId = "4fec8b3c5854e0f91f017a5e";
 /*
  * Gets the lists from a specific board
  */
-function getListsFromBoard(listId, cb) {
-  trello.get("/1/boards/" + listId + "/lists", {
-  }, function(err, res) {
-    console.log(res);
-  });
+function getListsFromBoard(boardId, filter, cb) {
+  trello.get("/1/boards/" + boardId + "/lists" + (filter ? ("/"+filter) : "" ), cb);
+}
+function getCardsFromBoard(boardId, filter, cb) {
+  trello.get("/1/boards/" + boardId + "/cards" + (filter ? ("/"+filter) : "" ), cb);
 }
 
-/*
- * This function creates a new card in the "Unsorted Milestones" list
- * for the milestone object passed to it.
- */
-function createUnsortedMilestoneCard(milestone, cb) {
-  var milestoneUrl = "https://github.com/" + config.user + "/" + config.repo + "/issues?milestone=" + milestone.number;
-  var description = milestoneUrl + '\n\n' + milestone.description;
-  trello.post("/1/cards", {
-    name: milestone.title,
-    desc: description,
-    idList: unsortedMilestonesListId
-  }, function(err, res) {
-    if(err) throw(err);
-    var id = res.id;
-    trello.post("/1/cards/" + id + "/checklists/", {
-      name: 'Issues'
-    }, function(err, res) {
-      if(err) throw err;
-      var checklistId = res.id;
-    });
-  });
-}
-
-github.issues.getAllMilestones({
-  user: config.user,
-  repo: config.repo,
-  state: "open"
-}, function(err, res) {
-  if(err) throw err;
-  res.sort(function(a,b) {
-    return b.number - a.number;
-  });
-
-  //createUnsortedMilestoneCard(res[0]);
-  for(var i = 0; i < res.length; ++i) {
-    console.log("#" + res[i].number + " - " + res[i].title);
-  }
-
-});
-
-/*
-github.events.getFromRepoIssues({
-  user: config.user,
-  repo: config.repo
-}, function(err, res) {
-  console.log(res);
-});
-*/
