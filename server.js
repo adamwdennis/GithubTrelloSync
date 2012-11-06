90b03b4b13ed0c9c82a0fce68eb3415c16f3ad7e
adamwdennis <adam.dennis@gmail.com>
2012-11-06 16:59:56 -0400

    cleanup

--- server.js
+++ server.js
@@ -14,6 +14,42 @@ github.authenticate({
     token: config.apiToken
 });
 
+var unsortedMilestonesListId = "5086f355c87793ab57000a86";
+var dailyBoardId = "4fec8b3c5854e0f91f017a5e";
+
+/*
+ * Gets the lists from a specific board
+ */
+function getListsFromBoard(listId, cb) {
+  trello.get("/1/boards/" + listId + "/lists", {
+  }, function(err, res) {
+    console.log(res);
+  });
+}
+
+/*
+ * This function creates a new card in the "Unsorted Milestones" list
+ * for the milestone object passed to it.
+ */
+function createUnsortedMilestoneCard(milestone, cb) {
+  var milestoneUrl = "https://github.com/" + config.user + "/" + config.repo + "/issues?milestone=" + milestone.number;
+  var description = milestoneUrl + '\n\n' + milestone.description;
+  trello.post("/1/cards", {
+    name: milestone.title,
+    desc: description,
+    idList: unsortedMilestonesListId
+  }, function(err, res) {
+    if(err) throw(err);
+    var id = res.id;
+    trello.post("/1/cards/" + id + "/checklists/", {
+      name: 'Issues'
+    }, function(err, res) {
+      if(err) throw err;
+      var checklistId = res.id;
+    });
+  });
+}
+
 github.issues.getAllMilestones({
   user: config.user,
   repo: config.repo,
@@ -24,23 +60,11 @@ github.issues.getAllMilestones({
     return b.number - a.number;
   });
 
-  var latestMilestone = res[0];
-  var description = latestMilestone.url + '\n\n' + latestMilestone.description;
-  trello.post("/1/cards", {
-    name: latestMilestone.title,
-    desc: description,
-    idList:"5086f355c87793ab57000a86"
-  }, function(err, res) {
-    if(err) throw(err);
-    var id = res.id;
-    trello.post("/1/cards/" + id + "/checklists/", {
-      name: 'Issues'
-    }, function(err, res) {
-      if(err) throw err;
-      var checklistId = res.id;
+  //createUnsortedMilestoneCard(res[0]);
+  for(var i = 0; i < res.length; ++i) {
+    console.log("#" + res[i].number + " - " + res[i].title);
+  }
 
-    });
-  });
 });
 
 /*
